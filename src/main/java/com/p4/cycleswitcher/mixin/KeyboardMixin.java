2ebaf4e4a06eff663f3fbb6a1b58befdc74df5d9
p4rty4nimAl <p4rty4nimAl@users.noreply.github.com>
2023-02-10 11:13:20 +0000

    Clean up KeyboardMixin; fix translation keys

--- src/main/java/com/p4/cycleswitcher/mixin/KeyboardMixin.java
+++ src/main/java/com/p4/cycleswitcher/mixin/KeyboardMixin.java
@@ -26,44 +26,35 @@ public class KeyboardMixin {
     @Inject(method = "processF3(I)Z", at = @At("RETURN"), cancellable = true)
     private void onProcessF3(int key, CallbackInfoReturnable<Boolean> cir) {
         switch (key) {
-            case 81: {
+            case 81 -> { // Q
                 ChatHud chatHud = MinecraftClient.getInstance().inGameHud.getChatHud();
-                chatHud.addMessage(Text.translatable("debug.weathers.help"));
-                chatHud.addMessage(Text.translatable("debug.times.help"));
-                chatHud.addMessage(Text.translatable("debug.difficulties.help"));
+                chatHud.addMessage(Text.translatable("debug.weather.help"));
+                chatHud.addMessage(Text.translatable("debug.time.help"));
+                chatHud.addMessage(Text.translatable("debug.difficulty.help"));
                 cir.setReturnValue(true);
-                return;
             }
-            case 294: {
+            case 294 -> { // F5
                 if (this.client.player.hasPermissionLevel(2)) {
                     this.client.setScreen(new SelectionScreen("weather", key));
-                } else addDebugMessage(Formatting.YELLOW, Text.translatable("debug.weathers.error"));
+                } else addDebugMessage(Formatting.YELLOW, Text.translatable("debug.weather.error"));
                 cir.setReturnValue(true);
-                return;
             }
-            case 295: {
+            case 295 -> { // F6
                 if (this.client.player.hasPermissionLevel(2)) {
                     this.client.setScreen(new SelectionScreen("time", key));
-                } else addDebugMessage(Formatting.YELLOW, Text.translatable("debug.times.error"));
+                } else addDebugMessage(Formatting.YELLOW, Text.translatable("debug.time.error"));
                 cir.setReturnValue(true);
-                return;
             }
-            case 296: {
+            case 296 -> { // F7
                 if (this.client.world.getLevelProperties().isDifficultyLocked()) {
-                    addDebugMessage(Formatting.YELLOW, Text.translatable("debug.difficulties.error2"));
+                    addDebugMessage(Formatting.YELLOW, Text.translatable("debug.difficulty.error2"));
                     cir.setReturnValue(true);
                     return;
                 }
                 if (this.client.player.hasPermissionLevel(2)) {
                     this.client.setScreen(new SelectionScreen("difficulty", key));
-                } else addDebugMessage(Formatting.YELLOW, Text.translatable("debug.difficulties.error"));
+                } else addDebugMessage(Formatting.YELLOW, Text.translatable("debug.difficulty.error"));
                 cir.setReturnValue(true);
-                return;
-            }
-            case 297: {
-                this.client.setScreen(new SelectionScreen("weather", key));
-                cir.setReturnValue(true);
-                return;
             }
         }
     }
